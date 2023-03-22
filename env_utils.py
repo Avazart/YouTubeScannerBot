7ad2c9876d332b9a6c5ea8d6f893626b0016f928
Avazart <avazart7@gmail.com>
2023-03-22 14:34:27 +0200

    Improve code in "env_utils.py". Add support for "dict" type

--- env_utils.py
+++ env_utils.py
@@ -1,18 +1,22 @@
+import dataclasses
 import os
-from dataclasses import dataclass, fields, is_dataclass, MISSING
 from functools import partial
 from pathlib import Path
 from types import GenericAlias
-from typing import Type, Any, Mapping, Iterable, get_origin, get_args
+from typing import Type, Any, Mapping, Iterable, get_origin, get_args, TypeAlias, MutableMapping, Generator
 
 _SIMPLE_TYPES = (bool, int, float, str, Path)
-_COLLECTION_TYPES = (list, set, frozenset, tuple)
+_COLLECTION_TYPES = (list, set, frozenset, tuple, dict)
+
+_ListOrSet = list | set | frozenset
+_ListOrSetType: TypeAlias = Type[_ListOrSet]
 
 
-@dataclass(frozen=True)
+@dataclasses.dataclass(frozen=True)
 class ParseConfig:
     el_sep: str = ';'
     cls_sep: str = '_'
+    kv_sep: str = ":"
     strip: bool = False
     skip_empty_parts: bool = False
     upper_case: bool = True
@@ -48,6 +52,19 @@ class CastValueError(EnvUtilsException):
                          f'can`t cast "{self.value}" to {self.type_name}!')
 
 
+def _split_str(s: str,
+               sep: str,
+               strip=False,
+               skip_empty_parts=False,
+               max_split=-1) -> list[str]:
+    parts = s.split(sep, max_split) if len(s) != 0 else []
+    if strip:
+        parts = map(str.strip, parts)
+    if skip_empty_parts:
+        parts = filter(None, parts)
+    return list(parts)
+
+
 def _str_to_bool(s: str) -> bool:
     """ This function convert string to boolean value """
     sl = s.lower().strip()
@@ -59,20 +76,8 @@ def _str_to_bool(s: str) -> bool:
         raise ValueError(f"Could not convert string to bool: '{s}'")
 
 
-def _split_string(s: str,
-                  sep=';',
-                  strip=False,
-                  skip_empty_parts=False) -> list[str]:
-    parts = s.split(sep) if len(s) != 0 else []
-    if strip:
-        parts = map(str.strip, parts)
-    if skip_empty_parts:
-        parts = filter(None, parts)
-    return list(parts)
-
-
-def _cast(value: str, target_type: Type, full_name: str) -> Any:
-    """ Cast string value to 'elementary' type """
+def _parse_simple(value: str, target_type: Type, name: str) -> Any:
+    """ Cast string value to 'simple' type """
     try:
         if issubclass(target_type, str):
             return value
@@ -81,7 +86,67 @@ def _cast(value: str, target_type: Type, full_name: str) -> Any:
         else:
             return target_type(value)
     except ValueError:
-        raise CastValueError(full_name, str(target_type), value)
+        raise CastValueError(name, str(target_type), value)
+
+
+def _parse_list_or_set(value: str,
+                       target_type,
+                       name: str,
+                       config: ParseConfig):
+    elements = _split_str(value,
+                          config.el_sep,
+                          config.strip,
+                          config.skip_empty_parts)
+    if arg_types := get_args(target_type):
+        parse = partial(_parse_simple, target_type=arg_types[0], name=name)
+        return target_type(map(parse, elements))
+    return target_type(elements)
+
+
+def _parse_tuple(value: str,
+                 target_type,
+                 name: str,
+                 config: ParseConfig):
+    elements = _split_str(value,
+                          config.el_sep,
+                          config.strip,
+                          config.skip_empty_parts)
+
+    if arg_types := get_args(target_type):
+        if len(elements) != len(arg_types):
+            raise CastValueError(name, str(target_type), value)
+
+        return target_type((
+            _parse_simple(element_value, element_type, name)
+            for element_value, element_type in zip(elements, arg_types)
+        ))
+
+    return target_type(elements)
+
+
+def _parse_items(s: str, config: ParseConfig) -> Generator[list[str], None, None]:
+    parts = _split_str(s,
+                       config.el_sep,
+                       config.strip,
+                       config.skip_empty_parts)
+    for s in parts:
+        yield _split_str(s,
+                         config.kv_sep,
+                         config.strip,
+                         config.skip_empty_parts,
+                         max_split=2)
+
+
+def _parse_dict(value: str,
+                target_type,
+                name: str,
+                config: ParseConfig) -> MutableMapping:
+    items = _parse_items(value, config)
+    if arg_types := get_args(target_type):
+        kt, vt = arg_types
+        items = ((_parse_simple(k, kt, name), _parse_simple(v, vt, name))
+                 for k, v in items)
+    return target_type(items)
 
 
 def _from_env(prefix: Iterable[str],
@@ -96,42 +161,31 @@ def _from_env(prefix: Iterable[str],
     if config.upper_case:
         full_name = full_name.upper()
 
-    value = env.get(full_name, MISSING)
-    if value is MISSING:
-        if default is MISSING:
+    value = env.get(full_name, dataclasses.MISSING)
+    if value is dataclasses.MISSING:
+        if default is dataclasses.MISSING:
             raise VariableRequired(full_name)
         else:
             return default
     elif issubclass(target_type, _SIMPLE_TYPES):
-        return _cast(value, target_type, full_name)
-    elif issubclass(target_type, _COLLECTION_TYPES):
-        return target_type(_split_string(value,
-                                         config.el_sep,
-                                         config.strip,
-                                         config.skip_empty_parts))
-    elif type(target_type) is GenericAlias:
-        collection_type = get_origin(target_type)
-        if issubclass(collection_type, (list, set, frozenset)):
-            element_type, *rest = get_args(target_type)
-            elements = _split_string(value,
-                                     config.el_sep,
-                                     config.strip,
-                                     config.skip_empty_parts)
-            cast = partial(_cast,
-                           target_type=element_type,
-                           full_name=full_name)
-            return collection_type(map(cast, elements))
-        elif issubclass(collection_type, tuple):
-            element_types = get_args(target_type)
-            elements = value.split(config.el_sep)
-            if len(elements) != len(element_types):
-                raise CastValueError(full_name, str(target_type), value)
-            values = []
-            for element_value, element_type in zip(elements, element_types):
-                values.append(_cast(element_value, element_type, full_name))
-            return tuple(values)
+        return _parse_simple(value, target_type, full_name)
+    else:
+        is_generic = type(target_type) is GenericAlias
+        origin_type = get_origin(target_type) if is_generic else target_type
+        if issubclass(origin_type, tuple):
+            return _parse_tuple(value, target_type, full_name, config)
+        elif issubclass(origin_type, dict):
+            return _parse_dict(value, target_type, full_name, config)
+        elif issubclass(origin_type, (list, set, frozenset)):
+            return _parse_list_or_set(value, target_type, full_name, config)
 
     raise TypeNotSupported(full_name, str(target_type))
+
+
+def _get_default(f: dataclasses.Field) -> Any:
+    return f.default \
+        if f.default_factory is dataclasses.MISSING \
+        else f.default_factory()
 
 
 def dataclass_from_env(dataclass_type,
@@ -146,14 +200,12 @@ def dataclass_from_env(dataclass_type,
         prefix = [prefix, ]
 
     attrs = {}
-    for f in fields(dataclass_type):
-        if is_dataclass(f.type):
+    for f in dataclasses.fields(dataclass_type):
+        if dataclasses.is_dataclass(f.type):
             attrs[f.name] = dataclass_from_env(f.type, (*prefix, f.name),
                                                env, config)
         else:
-            default = f.default \
-                if f.default_factory is MISSING \
-                else f.default_factory()
+            default = _get_default(f)
             attrs[f.name] = _from_env(prefix, f.name, f.type, default,
                                       env, config)
     return dataclass_type(**attrs)
--- tests/test_env_utils.py
+++ tests/test_env_utils.py
@@ -17,30 +17,35 @@ def prepare_env():
         "s": "test",
         "p": "C:/",
 
-        # lists
+        # list
         "b_list": "0;1;yes; no;on ;off",
         "i_list": "1; 2",
         "s_list": "a; b;c",
         "a_list": "1;a;3.14",
         "empty_list": "",
 
-        # sets
+        # set
         "b_set": "0;1;yes; no;on ;off",
         "i_set": "1; 2",
         "s_set": "a; b;c",
         "a_set": "1;a;3.14",
         "empty_set": "",
 
-        # frozenset s
+        # frozenset
         "b_frozenset": "0;1;yes; no;on ;off",
         "i_frozenset": "1; 2",
         "s_frozenset": "a; b;c",
         "a_frozenset": "1;a;3.14",
         "empty_frozenset": "",
 
-        # tuple s
+        # tuple
         "v": "ON;2;3.14;text",
         "va": "1;a",
+
+        # dict
+        "a_d": 'v1:a;v2:b',
+        "i_b_d": " 1:True;2:False",
+
         # nested
         "v1": "1",
         "b_v2": "2",
@@ -134,6 +139,18 @@ def test_tuple(prepare_env):
     assert e1 == e2
 
 
+def test_dict(prepare_env):
+    @dataclass
+    class D:
+        a_d: dict
+        i_b_d: dict[int, bool]
+    d1 = dataclass_from_env(D)
+    d2 = D(a_d={'v1': 'a', 'v2': 'b'},
+           i_b_d={1: True, 2: False})
+
+    assert d1 == d2
+
+
 def test_nested_classes(prepare_env):
     @dataclass
     class A:
@@ -163,10 +180,13 @@ def test_exceptions(prepare_env):
 
         dataclass_from_env(F)
 
+    class SomeClass:
+        pass
+
     with pytest.raises(TypeNotSupported):
         @dataclass
         class G:
-            v: dict
+            v: SomeClass
 
         dataclass_from_env(G)
 
