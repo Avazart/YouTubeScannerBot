843e5131c21cf2895c3f5cbb53992b57f6a97c85
Avazart <avazart7@gmail.com>
2025-06-07 16:36:48 +0300

    Fix run script

--- scripts/docker/run_app_prod_in_tmux.sh
+++ scripts/docker/run_app_prod_in_tmux.sh
@@ -4,7 +4,5 @@ if ! tmux has-session -t scanner; then
     tmux new -s scanner -d
     tmux send-keys -t scanner:0.0 "./run_app_prod.sh" Enter
     tmux split-window -t scanner:0.0 -v
-    tmux send-keys -t scanner:0.1 "cd /home/ubuntu/Projects/AdminHelper/AdminHelper" Enter
-    tmux send-keys -t scanner:0.1 "./run_bot.sh" Enter
 fi
     tmux attach -t scanner
